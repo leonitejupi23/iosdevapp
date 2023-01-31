de8e2815151ae93199b93d03bc8d96595e45e1aa
Leonit Ejupi <leonitejupi23gmail.com>
2023-01-31 15:10:38 +0200

    Initial Commit

---
+++ iosdevapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iosdevapp
+//
+//  Created by Leonit Ejupi on 31.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosdevapp/iosdevappApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosdevappApp.swift
+//  iosdevapp
+//
+//  Created by Leonit Ejupi on 31.01.23.
+//
+
+import SwiftUI
+
+@main
+struct iosdevappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
