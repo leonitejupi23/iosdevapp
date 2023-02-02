4e147b80097a6e7e4f9225997778ba8243fe6728
Leonit Ejupi <leonitejupi23gmail.com>
2023-02-02 15:06:30 +0200

    some of changes to UI

--- iosdevapp/ContentView.swift
+++ iosdevapp/ContentView.swift
@@ -5,17 +5,56 @@
 //  Created by Leonit Ejupi on 31.01.23.
 //
 
+
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var text: String = ""
+    let api = "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"
+    
+//    func BtnGenerate() {
+//        let
+//    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                
+                Image("Screenshot 2022-11-04 at 2.26.35 PM")
+                    .resizable()
+                    .frame(width: 300,height: 250)
+                    .aspectRatio(contentMode: .fill)
+                
+                TextField(
+                "Type to generate an image",
+                text: $text,
+                axis: .vertical
+                ).textFieldStyle(.roundedBorder)
+                
+                .multilineTextAlignment(
+                    .center)
+//                Color.red
+//                .background(Color.gray)
+                    .padding(50)
+                Button {
+                    Task {
+                        
+                    }
+                } label: {
+                    Text("Generate Images")
+                        .padding()
+                        .font(.title2)
+                        .buttonStyle(.borderedProminent)
+                        .background(.pink)
+                        .cornerRadius(10)
+                        .tint(.white)
+                }
+//                Spacer()
+            }
+            .navigationTitle("AI Image Generator")
+            .multilineTextAlignment(.leading)
         }
-        .padding()
     }
 }
 
